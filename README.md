5dc2803df9f297b398734538768950de16d21205
soulgalore <peter@soulgalore.com>
2017-09-19 10:25:02 +0200

    hello world

---
+++ README.md
@@ -0,0 +1,27 @@
+# Simulate network connections Linux and Mac OS X
+Inspired by [tylertreat/Comcast](https://github.com/tylertreat/Comcast), the [connectivity setting in the WPTAgent](https://github.com/WPO-Foundation/wptagent/blob/master/internal/traffic_shaping.py) and [sltc](https://github.com/sitespeedio/sltc).
+
+You can set the download/upload speed and RTT. Upload/download is in kbit/s and RTT in ms.
+
+## Start simulate a 3g network connection
+
+**throttle --up 330 --down 780 --rtt 200**
+
+## Stop simulate the 3g network connection
+**throttle --stop**
+
+## Add delay on your localhost (Linux only at the moment)
+
+**throttle --rtt 200 --localhost**
+
+## Stop adding delay on localhost
+**throttle --stop --localhost**
+
+## Use directly in NodeJS
+WIP.
+
+## Run in Docker (on Linux)
+
+Make sure to run: **sudo modprobe ifb numifbs=1** before you start the container.
+
+And then when you start your Docker container: *--cap-add=NET_ADMIN*
---
+++ bin/index.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const minimist = require('minimist');
+const throttler = require('../lib/');
+
+const defaultUp = 330;
+const defaultDown = 780;
+const defaultRtt = 200;
+
+const argv = minimist(process.argv.slice(2), {
+  boolean: ['stop', 'localhost']
+});
+
+if (argv.help) {
+  console.log('   Set the connectivity using the throttler (pfctl/tc)');
+  console.log('   Usage: throttler [options]');
+  console.log(
+    '   If you run in Docker throtler will only work on a Linux host'
+  );
+  console.log('   In Docker make sure to run: sudo modprobe ifb numifbs=1');
+  console.log('   And run your container with --cap-add=NET_ADMIN\n');
+  console.log('   Options:');
+  console.log('   --stop            Remove all settings');
+  console.log('   --up              Upload in Kbit/s ');
+  console.log('   --down            Download Kbit/s');
+  console.log('   --rtt             RTT in ms');
+} else {
+  if (argv.stop) {
+    const options = {
+      localhost: argv.localhost
+    };
+    throttler.stop(options).then(() => console.log('Stopped throttler'));
+  } else {
+    const options = {
+      up: argv.up || defaultUp,
+      down: argv.down || defaultDown,
+      rtt: argv.rtt || defaultRtt,
+      localhost: argv.localhost
+    };
+
+    throttler.start(options).then(() => {
+      if (options.localhost) {
+        console.log(`Started throttler on localhost RTT:${options.rtt}ms `);
+      } else {
+        console.log(
+          `Started throttler: Down:${options.down}kbit/s Up:${options.up}kbit/s RTT:${options.rtt}ms `
+        );
+      }
+    });
+  }
+}
