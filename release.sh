54ea11955776810b2f1d8b4c78e6f2da4a37ec7b
soulgalore <peter@soulgalore.com>
2017-09-19 10:56:12 +0200

    standard release script

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+
+# Super simple release script for sitespeed.io
+# Lets use it it for now and make it better over time :)
+# You need np for this to work
+# npm install --global np
+np $1
