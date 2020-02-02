13b6bcb80cb354660778132e2cddcfb936cc5448
Matt Hobbs <matt@nooshu.com>
2020-02-02 21:45:07 +0000

    Update the connectivity profiles to reflect those listed in WPT

--- bin/index.js
+++ bin/index.js
@@ -26,14 +26,49 @@ const profiles = {
     rtt: 200
   },
   '2g': {
-    down: 35,
-    up: 32,
-    rtt: 650
+    down: 280,
+    up: 256,
+    rtt: 400
   },
-  cable: {
+  'cable': {
     down: 5000,
     up: 1000,
     rtt: 14
+  },
+  'dsl': {
+    down: 1500,
+    up: 384,
+    rtt: 25
+  },
+  '3gem': {
+    down: 400,
+    up: 400,
+    rtt: 200
+  },
+  '4g': {
+    down: 9000,
+    up: 9000,
+    rtt: 85
+  },
+  'lte': {
+    down: 12000,
+    up: 12000,
+    rtt: 35
+  },
+  'edge': {
+    down: 240,
+    up: 200,
+    rtt: 420
+  },
+  'dial': {
+    down: 49,
+    up: 30,
+    rtt: 60
+  },
+  'fois': {
+    down: 20000,
+    up: 5000,
+    rtt: 2
   }
 };
 
