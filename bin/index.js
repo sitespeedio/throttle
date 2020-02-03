f968e8712d67e530cb40081b9950f618190b67a4
Matt Hobbs <matthew.hobbs@digital.cabinet-office.gov.uk>
2020-02-03 12:17:49 +0000

    Update the connectivity profiles to reflect those listed in WPT (#36)
    * Update the connectivity profiles to reflect those listed in WPT
    * Update connection speed for 2G

--- bin/index.js
+++ bin/index.js
@@ -34,6 +34,41 @@ const profiles = {
     down: 5000,
     up: 1000,
     rtt: 14
+  },
+  dsl: {
+    down: 1500,
+    up: 384,
+    rtt: 25
+  },
+  '3gem': {
+    down: 400,
+    up: 400,
+    rtt: 200
+  },
+  '4g': {
+    down: 9000,
+    up: 9000,
+    rtt: 85
+  },
+  lte: {
+    down: 12000,
+    up: 12000,
+    rtt: 35
+  },
+  edge: {
+    down: 240,
+    up: 200,
+    rtt: 420
+  },
+  dial: {
+    down: 49,
+    up: 30,
+    rtt: 60
+  },
+  fois: {
+    down: 20000,
+    up: 5000,
+    rtt: 2
   }
 };
 
