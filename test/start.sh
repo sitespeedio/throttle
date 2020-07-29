2f0fd5bedfb30ed472bada4fd23766bfb2d4330b
soulgalore <peter@soulgalore.com>
2020-07-29 21:01:29 +0200

    move

--- test/start.sh
+++ test/start.sh
@@ -2,7 +2,7 @@
 set -e
 
 ## Start/stop a couple of times
-/usr/src/app/bin/index.js --profile 3gslow
-/usr/src/app/bin/index.js --stop
-/usr/src/app/bin/index.js --profile cable
-/usr/src/app/bin/index.js --stop
+bin/index.js --profile 3gslow
+bin/index.js --stop
+bin/index.js --profile cable
+bin/index.js --stop
